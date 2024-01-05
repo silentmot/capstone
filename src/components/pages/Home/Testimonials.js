5d847c879c775f658edf3990c178391e58d0c5a9
Silentmot <mahmoud.tayeb@outlook.com>
2024-01-05 07:34:44 +0300

    Update testimonials in Home page

--- src/components/pages/Home/Testimonials.js
+++ src/components/pages/Home/Testimonials.js
@@ -10,29 +10,25 @@ const customers = [
     fullName: 'Maria Sanchez',
     image: customer1Image,
     rating: [1, 1, 1, 1, 0.5],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    says: `Dining at Little Lemon was a delightful experience! The ambiance was cozy and inviting, and the dishes were a burst of flavor. Each plate was a culinary masterpiece, leaving me eagerly anticipating my next visit. Little Lemon truly stands out as a gem in the city's dining scene.`,
   },
   {
     fullName: 'Antony Clifton',
     image: customer2Image,
     rating: [1, 1, 1, 1, 1],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    says: `Little Lemon has become my go-to restaurant for every special occasion. Their attention to detail, from the decor to the exceptional service, makes every meal memorable. The menu's variety caters to all tastes, and the quality of the food is consistently outstanding. A must-visit for anyone who loves great food`,
   },
   {
     fullName: 'Tamika Jackson',
     image: customer3Image,
     rating: [1, 1, 1, 1, 0.5],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    says: `The fusion cuisine at Little Lemon is simply extraordinary. Each dish is a unique blend of flavors that surprises and delights. It's rare to find a restaurant that balances innovation with classic cooking so well. The staff's warm hospitality adds to the overall exceptional dining experience. Highly recommended!`,
   },
   {
     fullName: 'Brandon Ming',
     image: customer4Image,
     rating: [1, 1, 1, 1],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    says: ` was thoroughly impressed with my experience at Little Lemon. The menu offers a perfect mix of traditional and contemporary dishes, all prepared to perfection. The ambiance is both elegant and relaxing, making it ideal for both business lunches and family dinners. Little Lemon has set a new standard for fine dining in the city.`,
   },
 ];
 
@@ -41,10 +37,10 @@ const Testimonials = () => {
     <section className="testimonials">
       <div className="container grid">
         <h2>What people say about us!</h2>
-        {customers.map((customer, index) => 
+        {customers.map((customer, index) =>
           <TestimonialCard key={index} customer={customer} />
         )}
-     </div>
+      </div>
     </section>
   );
 };
